a453613fecfc62acc959cfbdf398e8507f9da654
rodrigo.actavares@gmail.com <rodrigo.actavares@gmail.com>
2020-11-24 10:12:35 +0000

    Is Palindrome?

---
+++ IsPalindrome.py
@@ -0,0 +1,9 @@
+s = input ("Write the word to check: ")
+palindromeS = ""
+def isPalindrome (s):
+    isP = "Yes"
+    palindromeS = s[::-1]
+    if palindromeS != s:
+        isP = "No"
+    return isP
+print(isPalindrome(s))
